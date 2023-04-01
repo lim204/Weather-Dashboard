ef94708f86458997f7aa3cdbb10c226cec2ff722
Laura Mendez <lauramendez@Lauras-Air.lan>
2023-03-31 23:24:58 -0700

    navegation index and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 // console.log ('working')
 
-var apiKey ='7ab439372a6b7834b1058543aced3bee'
+// var apiKey ='7ab439372a6b7834b1058543aced3bee'
+var apiKey = '182957f0fa2946d78cf6ff810703352b';
 
-var searchWeather= document.querySelector('#searchweather')
-var searchInput = document.querySelector('input') 
+var searchWeather = document.querySelector('#searchweather');
+var searchInput = document.querySelector('input');
 
-function handleSearchSummit () {
-    if (!searchInput.value){
+function handleSearchSummit() {
+    if (!searchInput.value) {
         return;
     }
 
@@ -15,42 +16,42 @@ function handleSearchSummit () {
     searchInput.value = ""
 }
 
-function fetchWeather (city){
- var currentUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${city}&units=imperial`
+function fetchWeather(city) {
+    var currentUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${city}&units=imperial`
 
-    fetch ( currentUrl)
-    .then(response =>response.json())
-    .then(data=>{
-        console.log (data)
-        displayCurrentWeather (data)
-        var lat=data.coord.lat
-        var lon= data.coord.lon
-        var forcastUrl =`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
-    fetch (forcastUrl)
-    .then(response =>response.json())
-    .then(data=>{
-        console.log (data)
-        displayForcastWeather (data)
-    })
-    })
+    fetch(currentUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            displayCurrentWeather(data)
+            var lat = data.coord.lat
+            var lon = data.coord.lon
+            var forcastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+            fetch(forcastUrl)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    displayForcastWeather(data)
+                })
+        })
 }
-function displayCurrentWeather(data){
-    var temp= data.main.temp 
-    var hum= data.main.humidity
-    var wind= data.wind.speed
-    var date=data.dt
-    var city=data.name
-    document.getElementById('city').textContent=city
-    document.getElementById('date').textContent=date
-    document.getElementById('wind').textContent=wind
-    document.getElementById('hum').textContent=hum
-    document.getElementById('temp').textContent=temp
+function displayCurrentWeather(data) {
+    var temp = data.main.temp
+    var hum = data.main.humidity
+    var wind = data.wind.speed
+    var date = data.dt
+    var city = data.name
+    document.getElementById('city').textContent = city
+    document.getElementById('date').textContent = date
+    document.getElementById('wind').textContent = wind
+    document.getElementById('hum').textContent = hum
+    document.getElementById('temp').textContent = temp
 
 }
-function displayForcastWeather(data){
-    for (i=3; i <data.list.length; i+=8){
+function displayForcastWeather(data) {
+    for (i = 3; i < data.list.length; i += 8) {
         // createElement
     }
 
 }
-searchWeather.addEventListener('click',handleSearchSummit)
+searchWeather.addEventListener('click', handleSearchSummit)
